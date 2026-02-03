e37ac533163090cb41bb333b937434c9f46d84cd
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-03 19:11:54 +0545

    changes made

--- first_java/Main.java
+++ first_java/Main.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world");
+        System.out.println("Some thing to keep in mind for java:");
+        System.out.println("1. If you are making the class public then file name and class name must match");
+        
     }
 }
+
