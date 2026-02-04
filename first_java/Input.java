7947cf3c02ed7a19acb3046e6fc5bef97d806f20
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-04 23:24:05 +0545

    learnt about how to input strings and print them

---
+++ first_java/Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+   public static void main(String[] args) {
+       System.out.println("checking stuff");
+    // lets take a input
+    Scanner input = new Scanner(System.in);
+    System.out.println(input.next());
+    }
+}
+
--- first_java/Main.java
+++ first_java/Main.java
@@ -1,4 +1,4 @@
-import java.sql.SQLOutput;
+// import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args)
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Some thing to keep in mind for java:");
         System.out.println("1. If you are making the class public then file name and class name must match");
-        
+
     }
 }
 
