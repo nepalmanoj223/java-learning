e8bc5182c581d62d3146348902206d2dfb722372
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-05 17:49:31 +0545

    just working with the flow and since I have a history of coding in other languages  I tried functions in java but will do a deep dive later on !

--- first_java/Input.java
+++ first_java/Input.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 
 public class Input {
-   public static void main(String[] args) {
-       System.out.println("checking stuff");
-    // lets take a input
-    Scanner input = new Scanner(System.in);
-    System.out.println(input.next());
+    public static void main(String[] args){
+        Scanner in =new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1=in.nextInt();
+        int num2=in.nextInt();
+        System.out.println(multiply(num1,num2));
+    }
+    static int multiply(int num1, int num2){
+        // System.out.println("Enter fist num");
+        int mul=num1*num2;
+        return mul;   
     }
 }
+
 
