088c2f513b93c4a6fe25fd8457b6174cf1762323
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-14 17:54:59 +0545

    did ques no 2

--- Array/Chatgptques.java
+++ Array/Chatgptques.java
@@ -9,8 +9,8 @@ Write a program that takes an integer array and prints the sum of all elements.
 
 Example:
 
-Input: [1, 2, 3, 4]
-Output: 10
+Input: [something]
+Output: sum of that something
         */
        int [] ar={1,2,3,4,5,6,7,8,9};
        int sum=0;
@@ -19,6 +19,35 @@ Output: 10
        }
        System.out.println(sum);
 
+       nw();
+
+    }
+    static void nw(){
+        /*  
+           2. Largest and Smallest Element
+
+Find the largest and smallest number in a given integer array.
+
+Example:
+
+Input: [5, 1, 9, 3]
+Output: Largest = 9, Smallest = 1
+           */
+        int [] in= {9,14,12,15};
+        int smallest=in[0];
+        int largest=in[1];
+        for(int i=0; i<in.length; i++){
+            if (smallest>in[i]){
+                smallest=in[i];
+            }
+        }
+        System.out.println(smallest);
+         for(int i=0; i<in.length; i++){
+            if (largest<in[i]){
+                largest=in[i];
+            }
+        }
+        System.out.println(largest);
 
     }
 }
