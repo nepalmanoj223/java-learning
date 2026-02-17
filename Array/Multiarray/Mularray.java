5195b26a7ac9d10a83420fc2348e706b7d04132c
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-17 21:47:11 +0545

    2D array
    input of 2d array

---
+++ Array/Multiarray/Mularray.java
@@ -0,0 +1,34 @@
+package Array.Multiarray;
+
+import java.util.Scanner;
+
+public class Mularray {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        /*   
+        
+        123
+        456
+        789
+        */
+
+        /*  int[][]={
+        {},
+        {},
+        {}
+    }
+        or 
+        int[][] something= new int [2][];
+        */
+
+        //iput in 2D arrays
+
+        int [] [] arr= new int[3][3];
+        for(int row=0; row<3; row++){
+            for(int col=0; col<3; col++){
+                arr[row][col]=in.nextInt();
+            }
+        }
+
+    }
+}
