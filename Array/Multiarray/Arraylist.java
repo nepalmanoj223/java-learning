47e3715e9960474f804b8dba21bd7cab4a21db72
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-18 11:27:06 +0545

    list array

--- Array/Multiarray/Arraylist.java
+++ Array/Multiarray/Arraylist.java
@@ -18,8 +18,8 @@ public class Arraylist {
         list.add(1);
         list.add(1);
         System.out.println(list);
+// I have not included other tasks because every functions/methids of array list can be used and acessed when necessary
 
-        
     }
     
 }
